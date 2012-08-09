3c7123ff2979230bb12a4c1d0cccead6b8fc96ab
Muhammad Usman <muhammad@usmanity.com>
2012-08-09 11:39:16 -0700

    api call changed

--- photos/script.js
+++ photos/script.js
@@ -1,6 +1,6 @@
 $(function(){
   $.ajax({
-    url: 'http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=94549edc5a4a57eb27e08e164d359b6c&photoset_id=72157630981136960&extras=url_m&format=json&nojsoncallback=1&auth_token=72157630989475668-af0aa4a9f76f9bbe&api_sig=181d5fdb7de3bfca6904bbbdba9f4a11',
+    url: 'http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=94549edc5a4a57eb27e08e164d359b6c&photoset_id=72157630981136960&extras=url_m&format=json&nojsoncallback=1&api_sig=f6bd4d006fb891a184509a7b102c3571',
     success: function(data) {
       var photos = data.photoset.photo;
       console.log(photos)
