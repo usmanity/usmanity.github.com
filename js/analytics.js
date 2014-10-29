78dcba4515b2a43aa9034bbf17a2b7d83fb1d662
Muhammad Usman <muhammad@usmanity.com>
2014-10-29 15:55:35 -0700

    adding outbound

--- js/analytics.js
+++ js/analytics.js
@@ -5,3 +5,13 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-41779774-1', 'usmanity.com'); ga('send', 'pageview');
+
+
+var ob = new outbound('7d351997bb370d8f7788d211a8877fd8');
+var now = new Date;
+var userAttributes = {
+  attributes : {
+                   "date": now
+               }
+}
+ob.identify("user" + Math.random(), userAttributes);
