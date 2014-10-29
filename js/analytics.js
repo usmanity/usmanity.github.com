76d639a069a74cd859eddb8e81880a2a78207a71
Muhammad Usman <muhammad@usmanity.com>
2014-10-29 15:59:56 -0700

    adding outbound

--- js/analytics.js
+++ js/analytics.js
@@ -6,12 +6,4 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-41779774-1', 'usmanity.com'); ga('send', 'pageview');
 
-
-var ob = new outbound('7d351997bb370d8f7788d211a8877fd8');
-var now = new Date;
-var userAttributes = {
-  attributes : {
-                   "date": now
-               }
-}
-ob.identify("user" + Math.random(), userAttributes);
+window.outbound=window.outbound||[];window.outbound.methods=["identify","track","registerApnsToken","registerGcmToken","disableApnsToken","disableGcmToken"];window.outbound.factory=function(t){return function(){var o=Array.prototype.slice.call(arguments);o.unshift(t);window.outbound.push(o);return window.outbound}};for(var i=0;i<window.outbound.methods.length;i++){var key=window.outbound.methods[i];window.outbound[key]=window.outbound.factory(key)}if(!document.getElementById("outbound-js")){var script=document.createElement("script");script.type="text/javascript";script.id="outbound-js";script.async=true;script.src="//cdn.outbound.io/pub-7d8f3bb0e6613fd9bce6646bae622a6f.js";var first=document.getElementsByTagName("script")[0];first.parentNode.insertBefore(script,first)}
