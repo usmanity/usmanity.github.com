e45cd67e4cf2a91932561dfedd18c83cb58d58e4
Muhammad Usman <muhammad@usmanity.com>
2014-10-29 16:03:20 -0700

    more outbound config

--- js/analytics.js
+++ js/analytics.js
@@ -7,3 +7,6 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-41779774-1', 'usmanity.com'); ga('send', 'pageview');
 
 window.outbound=window.outbound||[];window.outbound.methods=["identify","track","registerApnsToken","registerGcmToken","disableApnsToken","disableGcmToken"];window.outbound.factory=function(t){return function(){var o=Array.prototype.slice.call(arguments);o.unshift(t);window.outbound.push(o);return window.outbound}};for(var i=0;i<window.outbound.methods.length;i++){var key=window.outbound.methods[i];window.outbound[key]=window.outbound.factory(key)}if(!document.getElementById("outbound-js")){var script=document.createElement("script");script.type="text/javascript";script.id="outbound-js";script.async=true;script.src="//cdn.outbound.io/pub-7d8f3bb0e6613fd9bce6646bae622a6f.js";var first=document.getElementsByTagName("script")[0];first.parentNode.insertBefore(script,first)}
+
+outbound.identify('user.' + Math.random(), {email: 'muhammad+testing@usmanity.com'}); 
+
