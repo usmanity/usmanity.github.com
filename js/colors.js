238867701d0c0ffdf02aab1d708ab08f06ec0e33
Muhammad Usman <muhammad@usmanity.com>
2014-11-03 15:46:57 -0800

    updating hexes

--- js/colors.js
+++ js/colors.js
@@ -1,4 +1,4 @@
-var getHexes = function(){
+var getDailyHex = function(){
   var d = new Date();
   var month = (d.getMonth() + 1).toString();
   var day = function(){ if (d.getDate() < 10) { return '0' + d.getDate() } return d.getDate().toString() };
@@ -7,9 +7,21 @@ var getHexes = function(){
   return dailyHex;
 }
 
-$(document).ready(function(){
-  var color = '#' + getHexes();
+var getSecondsHex = function(){
+  var d = new Date();
+  var hours = function(){ if (d.getHours() < 10) { return '0' + d.getHours() } return d.getHours().toString() };
+  var minutes = function(){ if (d.getMinutes() < 10) { return '0' + d.getMinutes() } return d.getMinutes().toString() };
+  var seconds = function(){ if (d.getSeconds() < 10) { return '0' + d.getSeconds() } return d.getSeconds().toString() };
+  return hours() + minutes() + seconds();
+}
+
+var updateColors = function(){
+  var color = '#' + getSecondsHex();
   $('body, a, #content #intro, ol li a').css({
     'color': color
   });
+}
+
+$(document).ready(function(){
+  window.setInterval(updateColors, 1000);
 });
