d89678003f37444d52d044d7092dfcb30ef3a2e2
Muhammad Usman <muhammad@usmanity.com>
2015-04-01 23:47:21 -0700

    updating to remove rotation

--- js/lastfm.js
+++ js/lastfm.js
@@ -1,6 +1,10 @@
 // count.js
 // get counts of things like last.fm scrobbles
 var a;
+var track;
+var spotify;
+var itunes;
+var amazon;
 usm = {
 	count: function(url, id) {
 		$.ajax({
@@ -9,10 +13,22 @@ usm = {
 	    	  switch (id) {
 	    	  	case "#nowPlaying":
 				      if (data.recenttracks.track[0]['@attr']) {
-                song = data.recenttracks.track[0].name + " by " +
+								track = data.recenttracks.track[0].name + " " + data.recenttracks.track[0].artist['#text'];
+                song = '"' + data.recenttracks.track[0].name + '" by ' +
 											"<a href='http://www.amazon.com/gp/search?ie=UTF8&camp=1789&creative=9325&index=music&linkCode=ur2&tag=usmanitycom0f-20&linkId=EB2WTXJOXLNBYR26&keywords=" +
 											data.recenttracks.track[0].artist['#text'] + "'>" + data.recenttracks.track[0].artist['#text'];
-                $(id)[0].innerHTML = "Now listening to " + song;
+                $(id)[0].innerHTML = 'Now listening to ' + song;
+								console.info("Now playing: " + track);
+
+								var url = 'https://api.spotify.com/v1/search?limit=1&type=track&query=' + track;
+								$.get(url, function(data){
+										spotifyResponse = data;
+										var trackId = spotifyResponse.tracks.items[0].uri;
+										spotify = $("<a href='" + trackId +"' class='open-in-spotify'>Spotify</a>");
+										console.log(trackId);
+										$(".links").append(spotify);
+								});
+								$(id).append('<div class="links"></div>');
               }
 							break;
 				    case "#lastfm":
@@ -37,4 +53,4 @@ usm = {
 	}
 }
 $(document).ready(usm.init);
-setInterval(usm.init, 10000);
+setInterval(usm.init, 100000);
