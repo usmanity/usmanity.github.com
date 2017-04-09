40dec931b8fdba039cab53cebed611ac0114356a
Muhammad Usman <muhammad@usmanity.com>
2017-04-09 01:00:28 -0700

    Lavendar 3.3 (#26)
    * adding yarn packaging + updating gitignore to not include node_modules
    
    * ditto
    
    * adding some colors
    
    * cleaning up css in homepage
    
    * moving to a simple grunt based setup
    
    * a little bit of reorganization

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		watch: {
+			sass: {
+				files: [
+					'scss/**/*.{scss,sass}'
+				],
+				tasks: ['sass:dist']
+			},
+			livereload: {
+				files: [
+					'*.html',
+					'js/**/*.{js,json}',
+					'css/*.css',
+					'img/**/*.{png,jpg,jpeg,gif,webp,svg}'
+				],
+				options: {
+					livereload: true
+				}
+			}
+		},
+		sass: {
+			options: {
+				sourceMap: true,
+				outputStyle: 'compressed'
+			},
+			dist: {
+				files: {
+					'css/homepage.css' : 'scss/homepage.scss'
+				}
+			}
+		}
+	});
+	grunt.registerTask('default', ['sass:dist', 'watch']);
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+};
