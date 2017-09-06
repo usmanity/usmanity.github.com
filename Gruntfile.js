4e94685f116f4d8cbdc7d72bf6bac51bc3c3bb31
Muhammad Usman <muhammad@usmanity.com>
2017-09-05 21:14:37 -0700

    resume 1.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,38 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		watch: {
-			sass: {
-				files: [
-					'scss/**/*.{scss,sass}'
-				],
-				tasks: ['sass:dist']
-			},
-			livereload: {
-				files: [
-					'*.html',
-					'js/**/*.{js,json}',
-					'css/*.css',
-					'img/**/*.{png,jpg,jpeg,gif,webp,svg}'
-				],
-				options: {
-					livereload: true
-				}
-			}
-		},
-		sass: {
-			options: {
-				sourceMap: true,
-				outputStyle: 'compressed'
-			},
-			dist: {
-				files: {
-					'css/styles.css' : 'scss/styles.scss',
-					'css/favorites.css' : 'scss/favorites.scss'
-				}
-			}
-		}
-	});
-	grunt.registerTask('default', ['sass:dist', 'watch']);
-	grunt.loadNpmTasks('grunt-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    watch: {
+      sass: {
+        files: ["scss/**/*.{scss,sass}"],
+        tasks: ["sass:dist"]
+      },
+      livereload: {
+        files: [
+          "*.html",
+          "js/**/*.{js,json}",
+          "css/*.css",
+          "img/**/*.{png,jpg,jpeg,gif,webp,svg}"
+        ],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    sass: {
+      options: {
+        sourceMap: true,
+        outputStyle: "compressed"
+      },
+      dist: {
+        files: {
+          "css/styles.css": "scss/styles.scss",
+          "css/favorites.css": "scss/favorites.scss",
+          "css/resume.css": "scss/resume.scss"
+        }
+      }
+    }
+  });
+  grunt.registerTask("default", ["sass:dist", "watch"]);
+  grunt.loadNpmTasks("grunt-sass");
+  grunt.loadNpmTasks("grunt-contrib-watch");
 };
