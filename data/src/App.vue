e3be6ced8e4ff68de418f03c473fa7f3e3f15bfb
Muhammad Usman <muhammad@usmanity.com>
2017-09-06 22:36:42 -0700

    working on a demo data project until that repo is in better shape

---
+++ data/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <ul class="cards">
+      <li  v-for="cardData in currentData">
+      <card>
+        <div class="test" slot="content">
+          <h1>{{ cardData.type }}</h1>
+          {{ cardData.latest }}
+          <strong>
+            {{ cardData.date }}
+          </strong>
+        </div>
+      </card>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Card from "./components/Card.vue";
+
+export default {
+  name: "app",
+  components: {
+    Card
+  },
+  data() {
+    return {
+      msg: "Welcome",
+      currentData: [
+        {
+          type: "movies",
+          latest: "The Big Sick",
+          date: "2017-09-05"
+        },
+        {
+          type: "weight",
+          latest: "131.4",
+          date: "2017-09-01"
+        },
+        {
+          type: "net-worth",
+          latest: "39835",
+          date: "2017-09-06"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ data/src/components/Card.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div class="card-wrapper">
+    <slot name="content"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "card"
+};
+</script>
+
+<style lang="scss">
+</style>
