65f08b05df95d372d33d3e5bee8fc431496cb617
Muhammad Usman <muhammad@usmanity.com>
2017-09-09 10:06:57 -0700

    adding backgroudn artifact

--- data/src/components/Card.vue
+++ data/src/components/Card.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <div class="card-wrapper">
-    <div class="background">
+    <div class="bg-artifact">
       {{ background }}
     </div>
     <h1> {{ title }}</h1>
@@ -17,11 +17,23 @@ export default {
 
 <style lang="scss">
 .card-wrapper {
-  border: 1px solid red;
   min-width: 200px;
   min-height: 200px;
   padding: 20px;
-  border-radius: 10px;
+  border-radius: 4px;
   background-color: white;
+  box-shadow: 0 0 30px rgba(12, 23, 34, 0.1);
+  margin: 0 20px;
+  color: #0074D9;
+  position: relative;
+  overflow: hidden;
+}
+.bg-artifact {
+  position: absolute;
+  font-size: 100px;
+  font-weight: bold;
+  color: rgba(200, 200, 200, 0.1);
+  bottom: -30px;
+    right: -10px;
 }
 </style>
