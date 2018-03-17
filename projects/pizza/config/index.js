ce12b4370ee40a329a042a509d0a6a895b871a93
Muhammad Usman <muhammad@usmanity.com>
2018-03-17 02:11:04 -0700

    moving paths

--- projects/pizza/config/index.js
+++ projects/pizza/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../order'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/projects/',
 
     /**
      * Source Maps
