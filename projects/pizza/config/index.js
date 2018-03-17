6ddba7736838879ffa69afb3d8a8797811141c2d
Muhammad Usman <muhammad@usmanity.com>
2018-03-17 02:19:48 -0700

    Adding the deployed folder

--- projects/pizza/config/index.js
+++ projects/pizza/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../order'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/projects/pizza',
+    assetsPublicPath: '/projects/pizza/order',
 
     /**
      * Source Maps
