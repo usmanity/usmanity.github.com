89431f6c397fa59b9954c0460b1c7cc3e8b6999c
Muhammad Usman <muhammad@usmanity.com>
2018-03-17 02:22:20 -0700

    redirect to orders now

--- projects/pizza/config/index.js
+++ projects/pizza/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../order'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/projects/pizza/order',
+    assetsPublicPath: '/projects/pizza/order/',
 
     /**
      * Source Maps
