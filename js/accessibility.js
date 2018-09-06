5f617d657fdcabff8f74d759790f93b14b460fbd
Muhammad Usman <muhammad@usmanity.com>
2018-09-05 23:28:45 -0700

    updating colors for night time

--- js/accessibility.js
+++ js/accessibility.js
@@ -1,8 +1,12 @@
 function initialize () {
     var date = new Date();
+    var body = document.getElementsByTagName('body')[0];
     if (date.getHours() < 7 || date.getHours() > 18) {
-        var body = document.getElementsByTagName('body')[0];
-        body.classList.add('dark')
+        body.classList.add('dark');
+        console.log(' 🌙 using dark color mode');
+    } else {
+        body.classList.add('light');
+        console.log(' ☀️ using light color mode');
     }
 }
 
