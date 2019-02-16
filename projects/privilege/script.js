0e6c4befdc4a11e1eb3ae20410f26d55505accf0
Muhammad Usman <muhammad@squareup.com>
2019-02-16 00:20:58 -0800

    a bunch of different project udpates

--- projects/privilege/script.js
+++
@@ -1,5 +0,0 @@
-var root = document.documentElement;
-window.addEventListener('deviceorientation', function(event) {
-    root.style.setProperty('--shadow-beta', (event.beta) + 'px');
-    root.style.setProperty('--shadow-gamma', (event.gamma) + 'px');
-});
