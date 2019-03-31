6c15fbd89ff81f432c9375a48ce228078598beca
Muhammad Usman <muhammad@fastmail.com>
2019-03-31 00:21:00 -0700

    site id changed

--- js/analytics.js
+++ js/analytics.js
@@ -1,3 +1,4 @@
+<script>
 (function(f, a, t, h, o, m){
 	a[h]=a[h]||function(){
 		(a[h].q=a[h].q||[]).push(arguments)
@@ -6,6 +7,6 @@
 	m=f.getElementsByTagName('script')[0];
 	o.async=1; o.src=t; o.id='fathom-script';
 	m.parentNode.insertBefore(o,m)
-})(document, window, '//analytics.usman.xyz/tracker.js', 'fathom');
-fathom('set', 'siteId', 'AMSSI');
+})(document, window, '//157.230.170.239/tracker.js', 'fathom');
+fathom('set', 'siteId', 'EMRHO');
 fathom('trackPageview');
