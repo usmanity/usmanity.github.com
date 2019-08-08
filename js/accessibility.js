72a96cfab8b67ad4f1db1037023e16088597af14
Muhammad Usman <muhammad@squareup.com>
2019-08-08 14:44:01 -0700

    a bit of a11y work changes

--- js/accessibility.js
+++ js/accessibility.js
@@ -1,13 +1,7 @@
 function initialize () {
     var date = new Date();
     var body = document.getElementsByTagName('body')[0];
-    // if (date.getHours() < 7 || date.getHours() > 18) {
-    //     body.classList.add('dark');
-    //     console.log(' 🌙 using dark color mode');
-    // } else {
-    //     body.classList.add('light');
-    //     console.log(' ☀️ using light color mode');
-    // }
+    console.log("Currently, no accessiblity features are enabled");
 }
 
 initialize();
