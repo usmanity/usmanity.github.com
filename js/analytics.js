0a44d4f9d49a06fc530d627ee32a29ec46997348
Muhammad Usman <muhammad@squareup.com>
2020-04-13 11:17:28 -0700

    remove analytics code from all project pages and homepage

--- js/analytics.js
+++ js/analytics.js
@@ -1,13 +1,3 @@
-(function(f, a, t, h, o, m){
-	a[h]=a[h]||function(){
-		(a[h].q=a[h].q||[]).push(arguments)
-	};
-	o=f.createElement('script'),
-	m=f.getElementsByTagName('script')[0];
-	o.async=1; o.src=t; o.id='fathom-script';
-	m.parentNode.insertBefore(o,m)
-})(document, window, '//stats.usman.xyz/tracker.js', 'fathom');
-fathom('set', 'siteId', 'EMRHO');
-fathom('trackPageview');
-
-console.log("Notes about analytics: all tracking is done by Fathom which means no user info is tracked past 'did someone show up on my site?'")
+console.log(
+  "Notes about analytics: There is no tracking done anymore. https://blog.usmanity.com/killing-analytics-on-my-personal-sites/"
+);
